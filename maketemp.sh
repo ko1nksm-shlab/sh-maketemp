59ea9af60889cb33260b23b93ac5d6512cf97bfc
Koichi Nakashima <koichi@nksm.name>
2025-01-31 00:10:44 +0900

    Changed random number algorithm to MINSTD

--- maketemp.sh
+++ maketemp.sh
@@ -14,14 +14,11 @@ maketemp() {
       [!-]*) set -- '' "$1" ;;
       *) echo "maketemp: unknown file type: $1" >&2 && exit 1
     esac
-    : & n=$! && wait $!
+    : & n=$(( ($! * 48271) % 2147483647 )) && wait $!
     retry=1000 && umask 0077
     while [ $((retry = retry - 1)) -ge 0 ]; do
-      n=$(( n ^ ((n << 13) & 4294967295) ))
-      n=$(( n ^ ((n >> 17) & 32767) ))
-      n=$(( n ^ ((n << 5) & 4294967295) ))
-      file=$(( (n < 0 ? -1 * n : n) % 100000000 + 100000000))
-      file="${TMPDIR:-/tmp}/${2:-tmp}.${file#1}"
+      n=$(( (n * 48271) % 2147483647 )) # MINSTD
+      file="${TMPDIR:-/tmp}/${2:-tmp}.${n#-}"
       "mk${1:-file}" "$file" 2>/dev/null && break
     done
     [ "$retry" -ge 0 ] && printf '%s\n' "$file" && exit 0
